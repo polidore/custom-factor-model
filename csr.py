eaff5e2f16a3891e7ab953c4151bc803890c6304
Ben Polidore <Benjamin.Polidore@atomlp.com>
2021-11-29 15:17:36 -0600

    basic python implementation.

---
+++ csr.py
@@ -0,0 +1,23 @@
+# %%
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+
+# %%
+data = pd.read_csv('./data.csv').iloc[:, 1:]
+data
+# %%
+cm = pd.read_csv('./constraints_matrix.csv')
+cm.rename(columns={'Unnamed: 0': 'factor_group'}, inplace=True)
+cm.set_index('factor_group', inplace=True)
+cm
+
+# %%
+glm = sm.GLM(data['total_return_1d'], data.iloc[:, 2:], freq_weights=data['wgt'])
+# %%
+res = glm.fit_constrained(constraints=(cm, np.zeros(shape=(len(cm)))))
+#res = glm.fit()
+res.summary()
+# %%
+
